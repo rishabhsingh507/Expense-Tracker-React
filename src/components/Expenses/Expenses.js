5451ac2261dc8717a37310535d8079377638d81a
rishabhsingh507 <rishabhsingh507@gmail.com>
2021-05-27 16:22:49 +0530

    inital commit

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+import ExpenseItem from './ExpenseItem'
+import './Expenses.css'
+import Card from '../Card/Card'
+import ExpensesFilter from './ExpensesFilter.js'
+
+const Expenses = (props) => {
+    const [fileteredYear, setFilteredYear] = useState('2020')
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    }
+    return (
+        <div>
+            <Card className="expenses">
+                <ExpensesFilter selected={fileteredYear} onChangeFilter={filterChangeHandler} />
+                <ExpenseItem
+                    title={props.items[0].title}
+                    amount={props.items[0].amount}
+                    date={props.items[0].date}
+                ></ExpenseItem>
+                <ExpenseItem
+                    title={props.items[1].title}
+                    amount={props.items[1].amount}
+                    date={props.items[1].date}
+                ></ExpenseItem>
+                <ExpenseItem
+                    title={props.items[2].title}
+                    amount={props.items[2].amount}
+                    date={props.items[2].date}
+                ></ExpenseItem>
+                <ExpenseItem
+                    title={props.items[3].title}
+                    amount={props.items[3].amount}
+                    date={props.items[3].date}
+                ></ExpenseItem>
+            </Card>
+        </div>
+    )
+}
+
+export default Expenses;
